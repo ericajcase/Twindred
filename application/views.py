20b96f0333765a05bfbe3caa95349bbef6dad91d
EricaJCase <ericajcase@gmail.com>
2017-07-27 08:01:56 -0700

    too much deployment fun

--- application/views.py
+++ application/views.py
@@ -26,7 +26,7 @@ def search_results(hashtag = None ):
     tweets = TweetCollection(hashtag)
     if len(tweets.tweetList)==0:
          flash('Search yielded no results!  Try a different search')
-         return render_template('search.html')
+         return render_template('search.html',title = 'Search', form = form)
 
 
     form = SimpleForm(request.form)
@@ -60,7 +60,7 @@ def like_minds():
 
     if len(tweets)==0:
          flash('Search yielded no results!  Try a different search')
-         return render_template('search,html')
+        return render_template('search.html',title = 'Search', form = form)
 
     hashtags = []
     locations = []
